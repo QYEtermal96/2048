753280fdbcdc29910cbe175d5f6fb0e1c6f406c3
e <1456958517@qq.com>
2017-06-10 14:40:39 +0800

    add randomNumber

--- index.js
+++ index.js
@@ -3,8 +3,11 @@
  */
 var app = new PIXI.Application(window.innerWidth, window.innerHeight, {backgroundColor: 0xffffff});
 document.body.appendChild(app.view);
+
+//var x,y;
+//if (window.innerHeight < 100) {}
+
 var basicText = new PIXI.Text('2048',{
-    align:"center",
     fontsize: 80
 });
 basicText.anchor.set(0.5);
@@ -27,6 +30,23 @@ for (var i = 0; i <4 ; i++) {
   }
 }
 
+function generateRandomNumber() {
+    return Math.floor(Math.random() * 4);
+}
+var x = generateRandomNumber();
+var y = generateRandomNumber();
 
+var graphics = new PIXI.Graphics();
+graphics.beginFill(0xff6666, 1);
+graphics.drawRect(app.renderer.width/6.5 + x*65, app.renderer.height/3 + y*65, 60, 60);
+app.stage.addChild(graphics);
 
+var RandomNumber = new PIXI.Text('2',{
+    fontsize: 100
+});
+
+RandomNumber.anchor.set(0.5);
+RandomNumber.x = app.renderer.width/6.5 + 30 + x*65;
+RandomNumber.y = app.renderer.height/3 + 30 +y*65;
+app.stage.addChild(RandomNumber);
 
